be08683af88bc38c152cd9b8c599e1d81cfbfe0a
Daniel Ayala <dayala@zerocoma.com>
2024-04-11 17:53:58 +0200

    change composer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:process.env.DB_HOST,
+      port:parseInt(process.env.DB_PORT),
+      username:process.env.DB_USERNAME,
+      password:process.env.POSTGRES_PASSWORD,
+      database:process.env.DB_NAME,
+      autoLoadEntities:true,// 
+      synchronize:true, //cuando hacemos un cambio en las entidades automaticamente las sincroniza, esto es bueno en dev
+    })
+  ],
   controllers: [],
   providers: [],
 })
